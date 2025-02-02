8c857d62d159eb31de61964e87da9057f5e77713
unknown <z_badawy@live.concordia.ca>
2025-02-01 23:25:31 -0500

    csv data

--- ConUHacksSAPdjango/import_data_csv.py
+++ ConUHacksSAPdjango/import_data_csv.py
@@ -14,10 +14,10 @@ client = MongoClient(uri)
 
 # Specify Database and Collection
 db = client['mydatabase']
-collection = db['backend_historicalfireevents']
+collection = db['backend_currentfireevents']
 
 # Specify CSV file to import
-csv_file = 'historical_wildfiredata.csv'
+csv_file = 'current_wildfiredata.csv'
 df = pd.read_csv(csv_file, sep=',')
 
 # Convert DataFrame to dictionary format for MongoDB
